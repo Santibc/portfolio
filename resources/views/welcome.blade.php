01d0919fc524002f5819554d93858a918626ff2e
michael.cardenas <michael.cardenas@realtechltda.com>
2025-07-01 14:18:00 -0500

    ss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -126,8 +126,13 @@
                     wrapper.find('.dataTables_info').addClass('text-sm text-gray-700 dark:text-gray-300');
 
                     // Style pagination buttons
-                    wrapper.find('.dataTables_paginate .paginate_button').addClass('relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-500 bg-white border border-gray-300 ms-1 hover:bg-gray-50 focus:outline-none disabled:opacity-50 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700 dark:hover:bg-gray-700 dark:hover:text-white rounded-md');
-                    wrapper.find('.dataTables_paginate .paginate_button.current').addClass('z-10 bg-indigo-600 text-white border-indigo-600 hover:bg-indigo-700 dark:bg-indigo-500 dark:text-white dark:border-indigo-500').removeClass('text-gray-500 bg-white dark:bg-gray-800 dark:text-gray-400');
+                   // Style pagination buttons
+                wrapper.find('.dataTables_paginate .paginate_button')
+                    .addClass('relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-500 bg-white border border-gray-300 ms-1 hover:bg-gray-50 focus:outline-none disabled:opacity-50 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700 dark:hover:bg-gray-700 dark:hover:text-white rounded-md');
+
+                wrapper.find('.dataTables_paginate .paginate_button.current')
+                    .addClass('z-10 bg-indigo-600 text-white border-indigo-600 hover:bg-indigo-700 dark:bg-indigo-500 dark:text-white dark:border-indigo-500')
+                    .removeClass('text-gray-500 bg-white dark:bg-gray-800 dark:text-gray-400');
                 }
             });
         });
