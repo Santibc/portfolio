c5d5bea4838bef0e922399d8ea5a5513c5e489b9
Nicolas Erazo <nico.lacho@outlook.com>
2026-01-08 17:50:46 -0500

    Update

--- resources/views/admin/tamanos-ramo/index.blade.php
+++ resources/views/admin/tamanos-ramo/index.blade.php
@@ -211,7 +211,21 @@ $(document).ready(function() {
     @if($tamanos->count() > 10)
         $('#tamanosTable').DataTable({
             language: {
-                url: '//cdn.datatables.net/plug-ins/1.13.7/i18n/es-ES.json'
+                "sProcessing": "Procesando...",
+                "sLengthMenu": "Mostrar _MENU_ registros",
+                "sZeroRecords": "No se encontraron resultados",
+                "sEmptyTable": "Ningún dato disponible en esta tabla",
+                "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+                "sSearch": "Buscar:",
+                "sLoadingRecords": "Cargando...",
+                "oPaginate": {
+                    "sFirst": "Primero",
+                    "sLast": "Último",
+                    "sNext": "Siguiente",
+                    "sPrevious": "Anterior"
+                }
             },
             order: [[0, 'asc']],
             pageLength: 25
