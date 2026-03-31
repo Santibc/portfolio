94618f3ac04dbcd72b2bc175fa3f7697da8a7d93
Mich Cardenas <michcardenas01@hotmail.com>
2026-03-31 11:08:17 -0500

    tags de adss

--- resources/views/landing_page/layout.blade.php
+++ resources/views/landing_page/layout.blade.php
@@ -592,6 +592,15 @@
     </style>
 
     @stack('styles')
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17792196133"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'AW-17792196133');
+    </script>
 </head>
 
 <body>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -122,6 +122,15 @@
             }
         }
     </style>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17792196133"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'AW-17792196133');
+    </script>
 </head>
 <body>
 
